5c0fdd1082d8006f4994e5e357a729fa34b8bde8
Michael Bowen <mikee.bowen@gmail.com>
2017-02-04 12:04:32 -0800

    build(jsdoc): add jsdoc documentation

--- local_modules/models/user/User.js
+++ local_modules/models/user/User.js
@@ -1,7 +1,14 @@
 'use strict';
+/**
+ * @module User model
+ * @exports local_modules/models/user/User.js
+ */
 
 const mongoose = require('mongoose');
-
+/**
+ * @todo Create functions to user schema to salt passwords
+ * @todo Create password field in user schema
+ */
 const userSchema = mongoose.Schema({
   userName: String,
   email: String,
