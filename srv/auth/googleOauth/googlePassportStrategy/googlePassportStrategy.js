bf8122323856ff2cf9e9ee59262fb4ada5dce0f7
Michael Bowen <mikee.bowen@gmail.com>
2017-02-05 12:46:33 -0800

    refactor(GoogltOauthPassportStrategy): switch to exporting GooglePassportOauthStrategy instance inst
    the module now directly exports the GoogleOauthOPassportStrategy instance directly instead of
    exporting a function that needs to be called to export the instance

--- srv/auth/googleOauth/googlePassportStrategy/googlePassportStrategy.js
+++ srv/auth/googleOauth/googlePassportStrategy/googlePassportStrategy.js
@@ -1,35 +1,37 @@
 'use strict';
-const Strategy = require('passport-google-oauth20').Strategy;
+const GooglePassportOauthStrategy = require('passport-google-oauth20').Strategy;
+const User = require('stories-map-models').User;
+
 let expressHost = process.env.NODE_ENV === 'development' || 'dev' ? `//localhost:${process.env.PORT}` : '';
 
-module.exports = function () {
-  return new Strategy({
+const googlePassportOauthStrategy =  new GooglePassportOauthStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: `${expressHost}/auth/login/google/return`,
     scope: ['profile', 'email']
   },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log('token', profile);
-      return cb(null, profile);
-
-      /*User.findOne({ googleId: profile.id }, (err, user) => {
-        if (err) return console.log(`Error : ${err}`)
-        if (!user) {
-          User.create({
-            displayName: profile.displayName,
-            givenName: profile.givenName,
-            familyName: profile.familyName,
-            emails: profile.emails,
-            googleId: profile.id
-          }, (err) => {
-            if (err) return console.log(`Error : ${err}`)
-            return cb(null, profile);
-   
-          });
-        } else if (user) {          
+  function (accessToken, refreshToken, profile, cb) {
+    console.log('token', profile);
+    /*User.findOne({ googleId: profile.googleId }, (err, user) => {
+      if (err) return console.log(`Error : ${err}`)
+      if (!user) {
+        User.create({
+          displayName: profile.displayName,
+          givenName: profile.givenName,
+          familyName: profile.familyName,
+          emails: profile.emails,
+          googleId: profile.id
+        }, (err) => {
+          if (err) return console.log(`Error : ${err}`)
           return cb(null, profile);
-        }
-      })*/
-    });
-};
+
+        });
+      } else if (user) {
+        return cb(null, profile);
+      }
+    })*/
+
+    return cb(null, profile);
+  });
+
+module.exports = googlePassportOauthStrategy;
