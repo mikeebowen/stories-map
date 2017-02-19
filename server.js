b1bb3e31dd78b5e96b19d5918a430ddfe7b12232
Michael Bowen <mikee.bowen@gmail.com>
2017-02-19 13:51:45 -0800

    feat(mongoDB): add authentication to connection with mongoDB
    mongoose now uses a username and password to authenticate before writing to the mongoDB database, the password and user name are stored in environment variables detailed in the .env.example file

--- server.js
+++ server.js
@@ -7,14 +7,24 @@ const morgan = require('morgan');
 const clc = require('cli-color');
 const mongoose = require('mongoose');
 const compression = require('compression');
+const format = require('util').format;
 const authRouter = express.Router();
 const db = mongoose.connection;
+
 const routes = require('./api/routes');
+
 let port = process.env.PORT;
 let serverStartTime = new Date();
-let mongoUri = process.env.MONGOLAB_URI;
+let encodedMongoDBPassword = encodeURIComponent(process.env.MONGODB_PASSWORD);
+let encodedMongoDBUsername = encodeURIComponent(process.env.MONGODB_USERNAME);
+let mongoDBPort = process.env.MONGODB_PORT;
+let mongoDBDatabase = process.env.MONGODB_DATABASE;
+let mongoDBHost = process.env.MONGODB_HOST;
+let mongoDBAuthMechanism = 'DEFAULT';
+let mongodbURI = format(`mongodb://%s:%s@${mongoDBHost}:${mongoDBPort}/${mongoDBDatabase}?authMechanism=%s`,
+  encodedMongoDBUsername, encodedMongoDBPassword, mongoDBAuthMechanism);
 
-mongoose.connect(mongoUri);
+mongoose.connect(mongodbURI, {auth:{authdb: mongoDBDatabase}});
 db.on('error', console.error.bind(console, 'connection error: '));
 db.on('open', (callback) => {
   console.log(clc.green('database connection made'));
